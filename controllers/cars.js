0bb2b33a6a4d1afa2e0a6941ef9e16d1e62bc4fb
ocbtomi36 <118051723+ocbtomi36@users.noreply.github.com>
2025-07-18 09:50:47 +0200

    update feature for cars is half ready

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,7 +1,9 @@
 
 const { validationResult } = require('express-validator');
 const Car = require('../models/cars');
-
+/**
+ * Gets all cars from the database
+ */
 exports.getCars = (req, res, next) => {
     Car.fetchAllCars()
     .then(([rows, field]) => {
@@ -14,11 +16,12 @@ exports.getCars = (req, res, next) => {
         })
     })
     .catch(err => {
-        console.error(err);
         res.status(500).json({ message: 'An error occurred' });
       });
 };
-
+/**
+ * Gets one car by the existing id
+ */
 exports.getCar = (req,res,next) => {
   const carId = req.params.carId;
   Car.findCarById(carId)
@@ -29,11 +32,10 @@ exports.getCar = (req,res,next) => {
     res.status(200).json(car);
   })
   .catch(err => {
-    console.error(err);
     res.status(500).json({message: 'An error occured'});
   });
 }
-// a routerbe a middleware-ben
+// Create a new car  
 exports.createCar = (req,res,next) => {
   const carName = req.body.type_of_car;
   const errors = validationResult(req);
@@ -49,16 +51,49 @@ exports.createCar = (req,res,next) => {
     } else {
       const type_of_car = req.body.type_of_car;
       const savedcar = new Car(type_of_car);
-      savedcar.save().then(() => res.status(201).json({ message: 'Car is created'}));
-      
+      savedcar.save().then(() => res.status(201).json({ message: 'Car is created'}));  
     }
   })
   .catch(err => {
-    console.error(err);
     res.status(500).json({message: 'An error occured'});
   });
 }
-exports.updateCar = (req,res,next) => {
-
+/**
+ * update one car by the id
+ */
+/** 
+ * Hogyan updatelünk ? 
+ * 1. Leellenörizzük, hogy a bejövő adatban van e hiba. Ha igen hiba akkor hibával térek vissza.
+ * 2. Leellenőrizzük, hogy a bejövő auto, ami egyedi létezik e az adatbázisban, ha igen akkor mi az azonosítója.
+ * Ha megegyezzik ami bejön pl 1 és 1 akkor nincs gond lehet updatelni.
+ * Ha nem hiba.
+ * Ha még nem szerepel az adatbázisban és jó a formátum k
+*/
+exports.updateCar = async (req,res,next) => {
+const paramId = req.params.carId;
+const incommingCarType = req.body.type_of_car;
+const errors = validationResult(req);
+const auto = new Car();
+  try {
+    if(!errors.isEmpty()){
+      return res.status(422).json({message: 'Validation failed.',
+            errors: errors.array() // a bejövő adatok validálása, nem üres és megfelel az adatbázisnak.
+    })}
+    const isValidId = await auto.asyncFindCarById(paramId);
+    console.log(isValidId)
+    if(isValidId === 0){
+      return res.status(422).json({message: 'Validation failed, there is no data with that id'})
+    }
+    const carDatasFromDb = await auto.asyncFindCarBytypeOfCar(incommingCarType);
+    const dataIdCars = carDatasFromDb.idcars.toString(); // adatbázisból
+    const dataTypeOfCar = carDatasFromDb.type_of_car; // adatbázisból
+    if(dataIdCars !== paramId && incommingCarType == dataTypeOfCar){
+      // itt azt kaptuk vissza, hogy az auto létezik az adatbázisban, de nem azzal az id-val amit beküldtek. Ez elutasítás
+      //Unique megszoriítás
+      res.status(422).json({ message: 'This type of car is already exists in db with different id' });
+      return;
+    }
+  } catch (error) {
+    res.status(500).json({message: 'An error occured'});
+  } 
 }
-
--- models/cars.js
+++ models/cars.js
@@ -14,7 +14,27 @@ module.exports = class Car {
     static findCarById(carId){
         return db.execute(' SELECT * FROM  my_mini_nodejs_app_db_github.cars where idcars = ?',[carId])
     }
-    static findCarBytypeOfCar(car){
-        return db.execute(' SELECT * FROM  my_mini_nodejs_app_db_github.cars where type_of_car = ?',[car])
+    // átírom asyc awaitre, mert kell az adatbázisból, hogy milyen autót tartalmaz.
+    async asyncFindCarBytypeOfCar(car){
+        try{
+        const [row] = await db.query(' SELECT * FROM  my_mini_nodejs_app_db_github.cars where type_of_car = ?',[car])
+        return row.length > 0 ? row[0] : 0;
+        } catch (err) {
+            throw new Error('Error in database query');
+        }
+    }
+    async asyncFindCarById(carId){
+        try{
+        const [row] = await db.query(' SELECT * FROM  my_mini_nodejs_app_db_github.cars where idcars = ?',[carId])
+        return row.length > 0 ? row[0] : 0;
+        
+        } catch (err) {
+            throw new Error('Error in database query')
+        }
+    }
+
+    
+    updateCarById(carId){
+        return db.execute('UPDATE users SET type_of_car = ? where idcars = ?',[this.type_of_car,carId])
     }
 };
